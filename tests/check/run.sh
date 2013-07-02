6a3f7a70abd24d17d20a8319c69d760735418d06
Qihoo360 <g-intra@360.cn>
2013-07-02 15:36:32 +0800

    remove unused files

--- tests/check/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LD_LIBRARY_PATH=/usr/local/mysql/lib/mysql
-
-#./check -t verify -c 1 -h 127.0.0.1 -P 4040 -u qtbuser -p qihoo.net -f sql_list
-#./check -t short -c 10 -h 127.0.0.1 -P 4050 -u zc -p zc
-./check -t long -c 10 -h 127.0.0.1 -P 4040 -u qtbuser -p qihoo.net -n 100000
--- tests/suite/base/t/bug_45167-test.lua
+++
@@ -1,4 +0,0 @@
----
--- just do nothing
---
-

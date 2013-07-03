6c5852a91f4b3bc69127b94eece1932ce83f571b
Qihoo360 <g-infra@360.cn>
2013-07-03 19:13:00 +0800

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 base=$(cd "$(dirname "$0")"; pwd)
 cd $base
-PKG_CONFIG_PATH=/usr/local/lib/pkgconfig ./configure --with-mysql=/usr --prefix=/usr/local/mysql-proxy CFLAGS="-DHAVE_LUA_H -O2" LDFLAGS="-lm -ldl -lcrypto" LUA_CFLAGS="/usr/local/bin/lua -I/usr/local/include/" LUA_LIBS="/usr/local/lib/ -llua"
+PKG_CONFIG_PATH=/usr/local/lib/pkgconfig ./configure --with-mysql=/usr --prefix=/usr/local/mysql-proxy CFLAGS="-DHAVE_LUA_H -O2" LDFLAGS="-lm -ldl -lcrypto" LUA_CFLAGS="-I/usr/local/include/" LUA_LIBS="-L/usr/local/lib -llua"
